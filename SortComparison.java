e580cf48b286888e8915a1c76898671c2922588f
Ben Burgess <burge337@morris.umn.edu>
2021-09-14 13:58:21 -0500

    Fix subsequence helper functions and actually call quicksort properly
    This should be the end of part 1 of the lab. Also includes comments for the counters of different element orderings.

--- SortComparison.java
+++ SortComparison.java
@@ -2,6 +2,78 @@
 
 import java.util.Arrays;
 
+// findings from count comparisons
+// Random ordering of elements:
+// Run 1
+// Counter for mergesort: 120346
+// Counter for quicksort: 157680
+// Run 2
+// Counter for mergesort: 120487
+// Counter for quicksort: 148904
+// Run 3
+// Counter for mergesort: 120303
+// Counter for quicksort: 150853
+// Run 4
+// Counter for mergesort: 120355
+// Counter for quicksort: 149532
+// Run 5
+// Counter for mergesort: 120386
+// Counter for quicksort: 159868
+
+
+// Increasing order of elements:
+// Run 1
+// Counter for mergesort: 9999
+// Counter for quicksort: 49995000
+// Run 2
+// Counter for mergesort: 9999
+// Counter for quicksort: 49995000
+// Run 3
+// Counter for mergesort: 9999
+// Counter for quicksort: 49995000
+// Run 4
+// Counter for mergesort: 9999
+// Counter for quicksort: 49995000
+// Run 5
+// Counter for mergesort: 9999
+// Counter for quicksort: 49995000
+
+
+// Increasing sequences of elements:
+// Run 1
+// Counter for mergesort: 10323
+// Counter for quicksort: 15005000
+// Run 2
+// Counter for mergesort: 10320
+// Counter for quicksort: 11011000
+// Run 3
+// Counter for mergesort: 10231
+// Counter for quicksort: 13006000
+// Run 4
+// Counter for mergesort: 10279
+// Counter for quicksort: 10015000
+// Run 5
+// Counter for mergesort: 10261
+// Counter for quicksort: 13014000
+
+
+// Decreasing sequences of elements:
+// Run 1
+// Counter for mergesort: 10211
+// Counter for quicksort: 26003000
+// Run 2
+// Counter for mergesort: 10313
+// Counter for quicksort: 12013000
+// Run 3
+// Counter for mergesort: 10199
+// Counter for quicksort: 12011000
+// Run 4
+// Counter for mergesort: 10249
+// Counter for quicksort: 9018000
+// Run 5
+// Counter for mergesort: 10232
+// Counter for quicksort: 11018000
+
 public class SortComparison {
 
     public static void main(String[] args) {
@@ -33,52 +105,38 @@ public class SortComparison {
 
     }
 
-    private void fillArrayRepeated(TestInteger[] toFill){
-        fillArrayRepeated(toFill, 1);
-    }
-
-    private void fillArrayRepeated(TestInteger[] toFill, int fillValue) {
-        Arrays.fill(toFill, new TestInteger(fillValue));
-    }
-
     private void fillArrayIncreasing(TestInteger[] toFill) {
-        for(int i = 0; i < toFill.length; i++){
+        for (int i = 0; i < toFill.length; i++) {
             toFill[i] = new TestInteger(i);
         }
     }
 
-    private void fillArrayDecreasing(TestInteger[] toFill) {
-        for(int i = 0; i < toFill.length; i++){
-            toFill[i] = new TestInteger(-1 * i);
-        }
-    }
-
-    private void fillArrayRandom(TestInteger[] toFill){
+    private void fillArrayRandom(TestInteger[] toFill) {
         fillArrayRandom(toFill, 1000000);
     }
 
     private void fillArrayRandom(TestInteger[] toFill, int upperBound) {
-        for(int i = 0; i < toFill.length; i++){
-            toFill[i] = new TestInteger((int)((java.lang.Math.random() * (upperBound - 1)) + 1));
+        for (int i = 0; i < toFill.length; i++) {
+            toFill[i] = new TestInteger((int) ((java.lang.Math.random() * (upperBound - 1)) + 1));
         }
     }
 
-    private void fillArrayRangeIncreasing(TestInteger[] toFill, int startPosition, int endPosition){
+    private void fillArrayRangeIncreasing(TestInteger[] toFill, int startPosition, int endPosition) {
         int baseRandom = ((int) ((java.lang.Math.random() * 999989) + 1));
-        for(int i = 0; i <= endPosition - startPosition; i++){
+        for (int i = 0; i <= endPosition - startPosition; i++) {
             toFill[i + startPosition] = new TestInteger(baseRandom + i);
         }
     }
 
-    private void fillArrayRangeDecreasing(TestInteger[] toFill, int startPosition, int endPosition){
+    private void fillArrayRangeDecreasing(TestInteger[] toFill, int startPosition, int endPosition) {
         Integer baseRandom = ((int) (java.lang.Math.random() * 999988));
-        for(int i = 0; i <= endPosition - startPosition; i++){
+        for (int i = 0; i <= endPosition - startPosition; i++) {
             toFill[startPosition + i] = new TestInteger(1000000 - (baseRandom + i));
         }
     }
 
-    private boolean isSorted(TestInteger[] toCheck){
-        for(int i = 0; i < toCheck.length - 1; i++){
+    private boolean isSorted(TestInteger[] toCheck) {
+        for (int i = 0; i < toCheck.length - 1; i++) {
             if (Integer.compare(toCheck[i].value, toCheck[i + 1].value) > 0) {
                 return false;
             }
@@ -86,24 +144,19 @@ public class SortComparison {
         return true;
     }
 
-    // for the future
-    // TestInteger pivot = toSort[rangeStart] //For lack of any knowledge about the array, take the first element as the pivot
-        // int pivotPosStart = 0;
-        // int pivotPosEnd = 0;
-
-    public void Quicksort(TestInteger[] A, int p, int r){
-        if(p < r){
-            int q = Partition(A, p, r - 1);
+    public void Quicksort(TestInteger[] A, int p, int r) {
+        if (p < r) {
+            int q = Partition(A, p, r);
             Quicksort(A, p, q - 1);
             Quicksort(A, q + 1, r);
         }
     }
 
-    public int Partition(TestInteger[] A, int p, int r){
+    public int Partition(TestInteger[] A, int p, int r) {
         TestInteger x = A[r];
         int i = p - 1;
-        for(int j = p; j < r; j++){
-            if(A[j].compareTo(x) <= 0){
+        for (int j = p; j < r; j++) {
+            if (A[j].compareTo(x) <= 0) {
                 i = i + 1;
                 exchange(A, i, j);
             }
@@ -112,7 +165,7 @@ public class SortComparison {
         return i + 1;
     }
 
-    private void exchange(TestInteger[] A, int p, int q){
+    private void exchange(TestInteger[] A, int p, int q) {
         TestInteger storage = A[p];
         A[p] = A[q];
         A[q] = storage;
@@ -120,68 +173,72 @@ public class SortComparison {
 
     // Comparisons
 
-    public void runComparisons(){
+    public void runComparisons() {
         System.out.println("Random ordering of elements:");
-        for(int i = 0; i < 5; i++){
+        for (int i = 0; i < 5; i++) {
             System.out.println("Run " + (i + 1));
             randomSortComparison();
         }
         System.out.println("Increasing order of elements:");
-        for(int i = 0; i < 5; i++){
+        for (int i = 0; i < 5; i++) {
             System.out.println("Run " + (i + 1));
             increasingSortComparison();
         }
         System.out.println("Increasing sequences of elements:");
-        for(int i = 0; i < 5; i++){
+        for (int i = 0; i < 5; i++) {
             System.out.println("Run " + (i + 1));
             subsequenceIncreasingSortComparison();
         }
         System.out.println("Decreasing sequences of elements:");
-        for(int i = 0; i < 5; i++){
+        for (int i = 0; i < 5; i++) {
             System.out.println("Run " + (i + 1));
             subsequenceDecreasingSortComparison();
         }
     }
 
-    private void randomSortComparison(){
+    private void randomSortComparison() {
         TestInteger[] toMergeSort = new TestInteger[10000];
         fillArrayRandom(toMergeSort);
         TestInteger[] toQuickSort = Arrays.copyOf(toMergeSort, toMergeSort.length);
         compareSorting(toMergeSort, toQuickSort);
     }
 
-    private void increasingSortComparison(){
+    private void increasingSortComparison() {
         TestInteger[] toMergeSort = new TestInteger[10000];
         fillArrayIncreasing(toMergeSort);
         TestInteger[] toQuickSort = Arrays.copyOf(toMergeSort, toMergeSort.length);
         compareSorting(toMergeSort, toQuickSort);
     }
 
-    private void subsequenceIncreasingSortComparison(){
+    private void subsequenceIncreasingSortComparison() {
         TestInteger[] toMergeSort = new TestInteger[10000];
-        for(int i = 0; i < 9000; i += 1000){
+        for (int i = 0; i < 10000; i += 1000) {
             fillArrayRangeIncreasing(toMergeSort, i, i + 999);
         }
         TestInteger[] toQuickSort = Arrays.copyOf(toMergeSort, toMergeSort.length);
         compareSorting(toMergeSort, toQuickSort);
     }
 
-    private void subsequenceDecreasingSortComparison(){
+    private void subsequenceDecreasingSortComparison() {
         TestInteger[] toMergeSort = new TestInteger[10000];
-        for(int i = 0; i < 9000; i += 1000){
+        for (int i = 0; i < 10000; i += 1000) {
             fillArrayRangeDecreasing(toMergeSort, i, i + 999);
         }
         TestInteger[] toQuickSort = Arrays.copyOf(toMergeSort, toMergeSort.length);
         compareSorting(toMergeSort, toQuickSort);
     }
 
-    private void compareSorting(TestInteger[] toMergeSort, TestInteger[] toQuickSort)
-    {
+    private void compareSorting(TestInteger[] toMergeSort, TestInteger[] toQuickSort) {
         Arrays.sort(toMergeSort);
-        System.out.println("Counter for mergesort: " + TestInteger.getCounter());
+        if (isSorted(toMergeSort)) {
+            System.out.println("Counter for mergesort: " + TestInteger.getCounter());
+        }
         TestInteger.resetCounter();
-        Quicksort(toQuickSort, 0, toQuickSort.length);
-        System.out.println("Counter for quicksort: " + TestInteger.getCounter());
+
+        Quicksort(toQuickSort, 0, toQuickSort.length - 1);
+        if (isSorted(toQuickSort)) {
+            System.out.println("Counter for quicksort: " + TestInteger.getCounter());
+        }
         TestInteger.resetCounter();
     }
 
